e513f7d4caa697231d1e1a6e7d14dfdecfab4068
AluriArun402 <aluri.freelancing@gmail.com>
2022-04-07 12:53:35 +0530

    First Commit

---
+++ src/components/HeaderBottom/HeaderBottom.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./HeaderBottom.css";
+import { BsSearch } from "react-icons/bs";
+
+function HeaderBottom() {
+  return (
+    <div className="flexWrapper">
+      <img src="https://www.aig.com/content/experience-fragments/aig/america-canada/us_corporate/en/us_corporate_header/master/_jcr_content/root/responsivegrid/responsivegrid/container_1423107018/image.coreimg.png/1628235494071/aig-logo.png" />
+
+      <div className="navbar">
+        <h5>INSURANCE</h5>
+        <h5>INVESTMENTS</h5>
+        <h5>CLAIMS</h5>
+      </div>
+      <div className="sideContainer">
+        <h5 className="loginSection">LOG IN</h5>
+        <BsSearch size="28px" />
+      </div>
+    </div>
+  );
+}
+
+export default HeaderBottom;
---
+++ src/components/HeaderTop/HeaderTop.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./HeaderTop.css";
+
+function HeaderTop() {
+  return (
+    <div className="Wrapper">
+      <div className="leftContainer">
+        <h6 className="text">INDIVIDUALS & FAMILIES</h6>
+        <h6>BUSINESS & ENTERPRISES</h6>
+      </div>
+
+      <div className="rightContainer">
+        <h6>NEWSROOM</h6>
+        <h6>CAREERS</h6>
+        <h6>INVESTORS</h6>
+        <h6>ABOUT US</h6>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderTop;
